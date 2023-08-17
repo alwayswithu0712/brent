f2e7bacbec810b1efd1aa1d1cd1964ae333fc273
Olaf-712 <devsirius44@gmail.com>
2023-08-17 07:59:29 +0200

    last

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(props){
+    const headString = props;
+
+    return(
+        <div className = ''>
+            {headString}
+        </div>
+    )
+}
